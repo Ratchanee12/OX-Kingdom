a6e0169b63b81152afe834018858f8f53e955e71
Peerawit Kongkitkul <peeratop02@gmail.com>
2023-04-05 01:50:19 +0700

    SignUpPage fully responsive, add music play function

--- public/jukebox.js
+++ public/jukebox.js
@@ -1,40 +1,43 @@
 // Get the current page URL
 const currentPage = window.location.href;
 
-console.log(currentPage);
-
 // Create a new audio element
 const audio = new Audio();
 
 // Check the current page and set the audio file to play
 if (currentPage.includes("SignUpPage_remastered.html")) {
-  audio.src = "https://example.com/huuray.mp3";
+    audio.src = "Sounds/Celtic_Fantasy.mp3";
 } else if (currentPage.includes("index.html")) {
-  audio.src = "https://example.com/ambient.mp3";
+    audio.src = "https://example.com/ambient.mp3";
 } else {
-  // default audio file to play if the current page is not recognized
-  audio.src = "https://example.com/default.mp3";
+    // default audio file to play if the current page is not recognized
+    audio.src = "https://example.com/default.mp3";
 }
 
 // Set the audio to loop and autoplay
 audio.loop = true;
 audio.autoplay = true;
 
-// Append the audio element to the body
-document.body.appendChild(audio);
+// Check if audio state is stored in localStorage
+const audioState = JSON.parse(localStorage.getItem('audioState'));
+if (audioState) {
+    audio.currentTime = audioState.time;
+    if (audioState.playing) {
+        audio.play();
+    }
+}
 
-// Create a volume control element
-const volumeControl = document.createElement("input");
-volumeControl.type = "range";
-volumeControl.min = 0;
-volumeControl.max = 1;
-volumeControl.step = 0.1;
-volumeControl.value = audio.volume;
-
-// Update the volume of the audio element when the volume control changes
-volumeControl.addEventListener("input", (event) => {
-  audio.volume = event.target.value;
+// Add an event listener to the audio element to detect when it's paused
+audio.addEventListener('pause', () => {
+    // Store the audio's current time and state (playing or paused) in localStorage
+    localStorage.setItem('audioState', JSON.stringify({time: audio.currentTime, playing: false}));
 });
 
-// Append the volume control element to the body
-document.body.appendChild(volumeControl);
+// Add an event listener to the audio element to detect when it's played
+audio.addEventListener('play', () => {
+    // Store the audio's current time and state (playing or paused) in localStorage
+    localStorage.setItem('audioState', JSON.stringify({time: audio.currentTime, playing: true}));
+});
+
+// Append the audio element to the body
+document.body.appendChild(audio);
