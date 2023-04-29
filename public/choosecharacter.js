4a2217bf2520c2fba8f54ac1abaac1185e90af55
It-63070036 <63070036@it.kmitl.ac.th>
2023-04-30 00:54:41 +0700

    Showing Essential Data in ChooseCharacter

--- public/choosecharacter.js
+++ public/choosecharacter.js
@@ -2,11 +2,17 @@ const characterBtn = document.querySelectorAll("#character-button");
 
 const roomRef = firebase.database().ref("RoomList");
 
+const userList = firebase.database().ref("UserList");
+
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 console.log(urlParams.get("roomid"));
 
 const roomData = firebase.database().ref(`RoomList/${urlParams.get("roomid")}`);
+
+let pictureCharacterO = document.getElementById("PlayercharacterO");
+
+let pictureCharacterX = document.getElementById("PlayercharacterX");
 
 characterBtn.forEach(el => {
     console.log(el.dataset.character);
@@ -24,11 +30,22 @@ function chooseCharacter(character){
         if(chooser.uid == snapshot.val().UserO){
             roomData.update({
                 CharacterO: character,
-            })   
+            })
+            if(snapshot.val().CharacterO == "Knight"){
+                pictureCharacterO.innerHTML = `${snapshot.val().CharacterO}`
+            } else if (snapshot.val().CharacterO == "Mage"){
+                pictureCharacterO.innerHTML = `${snapshot.val().CharacterO}`
+            }
+               
         } else if(chooser.uid == snapshot.val().UserX){
             roomData.update({
                 CharacterX: character,
             })
+            if(snapshot.val().CharacterX == "Knight"){
+                pictureCharacterO.innerHTML = `${snapshot.val().CharacterO}`
+            } else if (snapshot.val().CharacterX == "Mage"){
+                pictureCharacterO.innerHTML = `${snapshot.val().CharacterO}`
+            }
         }
     });
 }
@@ -36,11 +53,40 @@ function chooseCharacter(character){
 roomData.on("value", (snapshot) => {
     console.log("load data complete")
     let currentWatcher = firebase.auth().currentUser;
+    let uiduserO = snapshot.val().UserO;
+    let uiduserX = snapshot.val().UserX;
+    let usernameplayerO = "";
+    let usernameplayerX = "";
+    console.log("UidofUserO: ", uiduserO);
+    console.log("UidofUserX: ", uiduserX);
+    console.log("CurrentWatcher: ", currentWatcher.uid)
     document.getElementById("roomName").innerHTML = snapshot.val().LobbyName;
     if (currentWatcher.uid == snapshot.val().UserO || currentWatcher.uid == snapshot.val().UserX){
+        // if(currentWatcher.uid == uiduserO){
+        //     userList.child(uiduserO).once("value", userdata => {
+        //         document.getElementById("Username-PlayerO").innerHTML = `Player O: ${userdata.val().username}`;
+        //     }) 
+        // }else if(currentWatcher.uid == uiduserX){
+        //         userList.child(uiduserX).once("value", userdata => {
+        //             document.getElementById("Username-PlayerX").innerHTML = `Player X: ${userdata.val().username}`;
+        //         })
+        // }
+
+        if(currentWatcher.uid == uiduserO){
+            userList.child(uiduserO).once("value", userdata => {
+                console.log(userdata.val().username);
+                usernameplayerO = userdata.val().username;
+            }) 
+        }else if(currentWatcher.uid == uiduserX){
+            userList.child(uiduserX).once("value", userdata => {
+                usernameplayerX = userdata.val().username;
+            })
+        }
+        document.querySelector("Username-PlayerO").innerHTML = `Player O: ${usernameplayerO}`;
+        document.querySelector("Username-PlayerX").innerHTML = `Player X: ${usernameplayerX}`; 
+
 
     }else{
-        
         window.location= "Lobby.html";
     }
 });
@@ -65,6 +111,7 @@ function returntoLobby(){
                 console.log("UserO removed")
                 roomData.update({
                     UserO: "",
+                    CharacterO: "",
                 })
             }
         } else if (clickeruid == snapshot.val().UserX){
@@ -77,9 +124,19 @@ function returntoLobby(){
                 console.log("UserX removed")
                 roomData.update({
                     UserX: "",
+                    CharacterX: "",
                 })
             }
         }
     });
-    
+}
+
+function checkReady(){
+    var playerReady=false;
+    roomData.once("value", (snapshot) => {
+        if (snapshot.val().CharacterO != "" && snapshot.val().CharacterX != ""){
+            playerReady = true
+            //window.location = "Tictacttoe.html";
+        }
+    });
 }
