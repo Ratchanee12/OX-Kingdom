6796a6a9c37da859cd26a42ebfa9bd65b2e74bd8
It-63070036 <63070036@it.kmitl.ac.th>
2023-05-02 21:31:04 +0700

    Fix MyHeroJs and Html

--- public/MyHero.js
+++ public/MyHero.js
@@ -3,7 +3,7 @@ function pictureChange1()
     document.getElementById("MyHero1").src="https://github.com/Ratchanee12/OX-Kingdom/raw/63070149/public/Picture/HeroCharecter1.png";
     document.getElementById("NameHero").innerHTML = "Helix";
     document.getElementById("Des").innerHTML = "อัศวินทหารดาบแห่งเมือง Westeros<br>ที่มีพลังในการโจมตีสูง";
-    document.getElementById("skill").innerHTML = "+20% Attach Damage<br>+20% Healing";
+    document.getElementById("skill").innerHTML = "+20 Attack Damage<br>+20 Healing";
 }
 window.onload(pictureChange1)
 
